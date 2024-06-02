b7492567fe6911e537a18a99f0638c627bef21af
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-01 22:12:15 -0400

    Initial files added, theme acitvated, theme working

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Devs</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum vel quaerat est, quisquam harum molestiae ea voluptatum quam deserunt, recusandae doloribus quasi possimus, sed neque nobis quo officiis maiores.</p>
+</body>
+</html>
