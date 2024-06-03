9e90d55c471e57a4e851168c57f2427b59502220
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-02 22:09:05 -0400

    Stylesheets queued and css is working properly

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php 
 
 function guitararchive_load_scripts() {
-    
+    // Queue the stylesheet
+    wp_enqueue_style( 'guitararchive-style', get_stylesheet_uri(), array(), '1.0', 'all' );
 }
+
+// Add an action to load the load_scripts function when the page loads
+add_action( 'wp_enqueue_scripts', 'guitararchive_load_scripts' );
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum vel quaerat est, quisquam harum molestiae ea voluptatum quam deserunt, recusandae doloribus quasi possimus, sed neque nobis quo officiis maiores.</p>
