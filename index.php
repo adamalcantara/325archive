6f734bfa19ea0f1e4e6df328e8b23a7f2dc2187e
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-02 21:43:42 -0400

    Initial code for index.php added

--- index.php
+++ index.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Devs</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum vel quaerat est, quisquam harum molestiae ea voluptatum quam deserunt, recusandae doloribus quasi possimus, sed neque nobis quo officiis maiores.</p>
+    <div id="page" class="site">
+        <!-- The header of the website -->
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+
+                </div>
+                <!-- The menu -->
+                <div class="menu-area">
+                    <nav class="main-menu">
+
+                    </nav>
+                </div>
+            </section>
+        </header>
+        <!-- Dynamic content area -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <!-- Hero element -->
+                    <section class="hero">
+
+                    </section>
+                    <!-- Mission section -->
+                    <section class="mission">
+                        
+                    </section>
+                    <!-- Home blog -->
+                    <section class="home-blog">
+
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- Footer -->
+        <footer class="site-footer">
+
+        </footer>
+    </div>
 </body>
 </html>
