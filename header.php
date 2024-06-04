8345c8317bf3c35239f8f1c054f810b531188c17
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-03 23:07:20 -0400

    Button added for mobile menu and styled

--- header.php
+++ header.php
@@ -16,6 +16,13 @@
                     <!-- The menu -->
                     <div class="menu-area">
                         <nav class="main-menu">
+                            <button class="check-button">
+                                <div class="menu-icon">
+                                    <div class="bar1"></div>
+                                    <div class="bar2"></div>
+                                    <div class="bar3"></div>
+                                </div>
+                            </button>
                             <?php wp_nav_menu( array( 'theme_location' => 'guitararchive_main_menu', 'depth' => 2 )); ?>
                         </nav>
                     </div>
