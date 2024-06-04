b584b277fb19cb3e35389b00660b89ff46005558
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-03 22:55:27 -0400

    Footer menu added and styled

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
         <!-- Footer -->
     </div>
     <footer class="site-footer">
-        Footer
+
+            <div class="copyright">
+                <p>Copyright X - All Rights Reserved</p>
+            </div>
+            <nav class="footer-menu">
+                <?php wp_nav_menu( array( 'theme_location' => 'guitararchive_footer_menu', 'depth' => 1 )); ?>
+            </nav>
+
     </footer>
 <?php wp_footer(); ?>
 </body>
