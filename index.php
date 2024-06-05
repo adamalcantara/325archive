ae2809eb137dd4f11b90401a53394ecfb4d8734c
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-05 00:43:03 -0400

    Permalinks added

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
                                     ?>
                                     <article class="blog-item">
                                         <p class="blog-date"><?php echo get_the_date(); ?></p>
-                                        <h2 class="blog-title"><?php the_title(); ?></h2>
-                                        <?php the_post_thumbnail( 'full' ); ?>
-                                        <?php the_content(); ?>
+                                        <h2 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'full' ); ?></a>
+                                        <?php the_excerpt(); ?>
                                     </article>
                                     <?php
                                 endwhile;
--- page-home.php
+++ page-home.php
@@ -35,8 +35,8 @@
                                         ?>
                                         <article class="home-blog-item">
                                             <p class="blog-date"><?php echo get_the_date(); ?></p>
-                                            <h2 class="blog-title"><?php the_title(); ?></h2>
-                                            <?php the_post_thumbnail( 'full' ) ?>
+                                            <h2 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'full' ) ?></a>
                                             <?php the_excerpt(); ?>
                                         </article>
                                         <?php
