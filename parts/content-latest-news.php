7341ee32bb7df69cd706404e6247bf85f434bfa5
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-24 00:49:49 -0400

    Home blog items turned into cards

--- parts/content-latest-news.php
+++ parts/content-latest-news.php
@@ -1,6 +1,12 @@
 <article class="home-blog-item">
-    <p class="blog-date"><?php echo get_the_date(); ?></p>
-    <h2 class="home-blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'full' ) ?></a>
-    <?php the_excerpt(); ?>
+    <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail( 'full' ) ?>
+    </a>
+    <div class="home-blog-info">
+        <h2 class="home-blog-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>
+        <?php the_excerpt(); ?>
+    </div>
+    <p class="home-blog-date"><?php echo get_the_date(); ?></p>
 </article>
